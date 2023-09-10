ac04183eeb841b0abe045d4bf32d2b34c36e5c87
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2023-09-10 10:17:09 -0500

    change scrollbar size

--- packages/style/src/shared/scrollbars.css.ts
+++ packages/style/src/shared/scrollbars.css.ts
@@ -4,13 +4,13 @@ import { contract } from "../theme";
 export const scrollbars = style({
   scrollbarWidth: "thin",
   scrollbarColor: `${contract.color.neutral[4]} ${contract.color.neutral[5]}`,
-  marginRight: 4,
+  // marginRight: 12,
 
   selectors: {
     // Chrome, Edge, and Safari
     "&::-webkit-scrollbar": {
-      width: 4,
-      height: 4,
+      width: 2,
+      height: 2,
     },
     "&::-webkit-scrollbar-track": {
       background: "transparent",
