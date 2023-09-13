cae87f9239ff0e536b07e96a1b23828981554d80
Yoav Lavi <yoav@yoavlavi.com>
2023-09-13 19:03:27 +0300

    updates

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import monacoEditorPlugin from "vite-plugin-monaco-editor";
 
 export default defineConfig(({ command }) => {
   return {
@@ -28,6 +29,19 @@ export default defineConfig(({ command }) => {
     envPrefix: ["VITE_", "TAURI_"],
     plugins: [
       react(),
+      (monacoEditorPlugin as any).default({
+        customDistPath: (_root, buildOutDir) => {
+          // this ensures that our workers will be copied to the default folder (monacoeditorwork) next to /assets in the build dir
+          return buildOutDir + "/" + "monacoeditorwork";
+        },
+        languageWorkers: ["json", "editorWorkerService"],
+        customWorkers: [
+          {
+            label: "graphql",
+            entry: "monaco-graphql/esm/graphql.worker",
+          },
+        ],
+      }),
       vanillaExtractPlugin({
         identifiers: "debug",
         emitCssInSsr: true,
