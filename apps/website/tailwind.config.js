84428ddc7ed44885861c7b076ff2babaa66bfcb9
jvidalv <josepvidalvidal@gmail.com>
2023-09-14 13:39:56 +0200

    feat: coming soon landing page

--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -1,4 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.astro']
+  content: ['./src/**/*.astro'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
 }
