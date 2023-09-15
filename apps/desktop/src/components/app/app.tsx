7e49cf40563f2b81a9bfe674055ca3633e014f56
Yoav Lavi <yoav@yoavlavi.com>
2023-09-15 14:33:17 +0300

    visual tweaks

--- apps/desktop/src/components/app/app.tsx
+++ apps/desktop/src/components/app/app.tsx
@@ -2,7 +2,7 @@ import { Pathfinder, Trailblazer } from "@pathfinder/core";
 import { useState } from "react";
 
 export const App = () => {
-  const [url, setUrl] = useState("https://graphql.earthdata.nasa.gov/api");
+  const [url, setUrl] = useState("https://api.react-finland.fi/graphql");
 
   return (
     <div style={{ height: "100%", width: "100%", padding: 0, margin: 0 }}>
@@ -12,32 +12,44 @@ export const App = () => {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          height: "40px",
+          height: "45px",
           width: "100%",
         }}
       >
         <input
           value={url}
           placeholder="Enter a URL..."
-          onChange={(event) => setUrl(event.target.value)}
+          onChange={(event) => {
+            if (event.target.value !== url) {
+              setUrl(event.target.value);
+            }
+          }}
           style={{
-            fontSize: "13px",
-            padding: "5px",
+            fontSize: "14px",
+            fontWeight: 500,
+            fontFamily:
+              "Inter, -apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, Ubuntu, roboto, noto, segoe ui, arial, sans-serif",
+            padding: "7px",
             width: "75%",
-            color: "rgb(177,177,177)",
-            backgroundColor: "#00000033",
-            borderRadius: "5px",
-            border: "1px solid rgb(43,42,42)",
+            color: "rgb(191,191,191)",
+            backgroundColor: "rgb(24,24,24)",
+            borderRadius: "8px",
+            border: "1px solid transparent",
           }}
         />
       </div>
       <hr style={{ border: "0.1px solid rgb(43,42,42)" }} />
-      <div style={{ height: "calc(100% - 40px)" }}>
+      <div style={{ height: "calc(100% - 45px)" }}>
         <Trailblazer
           schemaProps={{
             fetcherOptions: {
               endpoint: url,
-              headers: [],
+              headers: [
+                {
+                  key: "Content-Type",
+                  value: "application/json",
+                },
+              ],
             },
           }}
         >
