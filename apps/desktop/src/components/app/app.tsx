ad2dc38f79f396868932f040a703ce4200ab535b
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2023-09-20 13:48:36 -0700

    add the schema view to desktop app

--- apps/desktop/src/components/app/app.tsx
+++ apps/desktop/src/components/app/app.tsx
@@ -1,10 +1,14 @@
-import { Trailblazer } from "@pathfinder/core";
+import { PluginSchemaAwareSchemaView, Trailblazer } from "@pathfinder/core";
 import { appContainer } from "./app.css";
 
 export const App = () => {
   return (
     <div className={appContainer}>
-      <Trailblazer />
+      <Trailblazer 
+            plugins={{
+              schemaAwarePlugins: [PluginSchemaAwareSchemaView],
+            }}
+      />
     </div>
   );
 };
