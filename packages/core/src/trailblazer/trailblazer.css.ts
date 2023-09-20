7d91ea82e274839f6b7029c76181e16a92dd421b
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2023-09-20 12:06:57 -0700

    add proper background color to Trailblazer css

--- packages/core/src/trailblazer/trailblazer.css.ts
+++ packages/core/src/trailblazer/trailblazer.css.ts
@@ -1,13 +1,15 @@
-import { style } from "@pathfinder/style";
+import { contract, style } from "@pathfinder/style";
 
 export const trailblazerClass = style({
   height: "100%",
   width: "100%",
+  backgroundColor: contract.color.neutral[1],
 });
 
 export const connectWrapClass = style({
   height: "100%",
   width: "100%",
+  backgroundColor: contract.color.neutral[1],
   paddingLeft: "25%",
   paddingRight: "25%",
   display: "flex",
