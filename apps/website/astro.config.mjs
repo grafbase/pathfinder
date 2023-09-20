af71cabccf80a48f4b643f9aecfbdb04dcfa3f10
jvidalv <josepvidalvidal@gmail.com>
2023-09-20 16:18:16 +0200

    feat: live pathfinder

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,8 +1,30 @@
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react';
-import { defineConfig } from 'astro/config'
+import reactVite from "@vitejs/plugin-react";
+import {defineConfig} from "vite";
+import {vanillaExtractPlugin} from "@vanilla-extract/vite-plugin";
+import monacoEditorPlugin from "vite-plugin-monaco-editor";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+    integrations: [tailwind(), react()],
+    vite: {
+        plugins: [
+            reactVite(),
+            monacoEditorPlugin.default({
+                customDistPath: (_root, buildOutDir) => {
+                    // this ensures that our workers will be copied to the default folder (monacoeditorwork) next to /assets in the build dir
+                    return buildOutDir + "/" + "monacoeditorwork";
+                },
+                languageWorkers: ["json", "editorWorkerService"],
+                customWorkers: [
+                    {
+                        label: "graphql",
+                        entry: "monaco-graphql/esm/graphql.worker",
+                    },
+                ],
+            }),
+            vanillaExtractPlugin({}),
+        ],
+    }
 })
