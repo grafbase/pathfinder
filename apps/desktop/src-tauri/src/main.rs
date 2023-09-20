3e1475cdb4146fac431638d1ef3003eb9735b304
Yoav Lavi <yoav@yoavlavi.com>
2023-09-20 19:13:44 +0300

    fixes

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -1,24 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
-
 fn main() {
     tauri::Builder::default()
-        .setup(|app| {
-            let window = app.get_window("main").unwrap();
-
-            #[cfg(target_os = "macos")]
-            window_vibrancy::apply_vibrancy(
-                &window,
-                window_vibrancy::NSVisualEffectMaterial::HudWindow,
-                None,
-                None,
-            )
-            .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
-
-            Ok(())
-        })
         .run(tauri::generate_context!())
         .expect("error while running Pathfinder");
 }
--- apps/desktop/src/components/app/app.css.ts
+++ apps/desktop/src/components/app/app.css.ts
@@ -1,22 +1,8 @@
-import { style, globalStyle } from "@pathfinder/style";
+import { style } from "@pathfinder/style";
 
 export const appContainer = style({
   display: "block",
   height: "100%",
   padding: 0,
   margin: 0,
-});
-
-const DARWIN = process.platform == "darwin";
-
-globalStyle("html", {
-  background: "rgb(35,35,35) !important",
-
-  "@media": {
-    "(prefers-color-scheme:dark)": {
-      background: DARWIN
-        ? "rgba(35, 35, 35, 0.85) !important"
-        : "rgb(35,35,35) !important",
-    },
-  },
 });
