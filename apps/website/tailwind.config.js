0c68f10473ec79927627f991ac57f3914d4891cb
jvidalv <josepvidalvidal@gmail.com>
2023-09-21 16:15:25 +0200

    fix: tailwind

--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.astro'],
+  content: ['./src/**/*.{astro,tsx}'],
   theme: {
     extend: {
       fontFamily: {
