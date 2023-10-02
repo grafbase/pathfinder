3725f195d14ca459d0b90f8d1fc0fd43a492f672
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2023-10-02 10:02:53 -0500

    use relative path for component import

--- apps/desktop/src/components/app/app.tsx
+++ apps/desktop/src/components/app/app.tsx
@@ -1,4 +1,10 @@
-import { PluginSchemaAwareSchemaView, Trailblazer } from "@pathfinder/core";
+// relative path import here to src components
+// this allows us to dev without the build step and forgo importing the built css file
+import {
+  PluginSchemaAwareSchemaView,
+  Trailblazer,
+} from "../../../../../packages/core/src";
+
 import { appContainer } from "./app.css";
 
 export const App = () => {
