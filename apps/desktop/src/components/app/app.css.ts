aebc2cadefea03d1a3fa68ad644b1ebc57fa175b
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2023-10-21 12:19:45 -0500

    run prettier write in apps/desktop

--- apps/desktop/src/components/app/app.css.ts
+++ apps/desktop/src/components/app/app.css.ts
@@ -1,12 +1,12 @@
-import { style, globalStyle } from "@pathfinder-ide/style";
+import { style, globalStyle } from '@pathfinder-ide/style';
 
 export const appContainer = style({
-  display: "block",
-  height: "100%",
+  display: 'block',
+  height: '100%',
   padding: 0,
   margin: 0,
 });
 
-globalStyle("html", {
-  background: "rgb(35,35,35)",
+globalStyle('html', {
+  background: 'rgb(35,35,35)',
 });
