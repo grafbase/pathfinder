0f55c29f43cbd2e1523499d096a48506f502b966
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2023-10-21 12:20:41 -0500

    run prettier write in apps/website

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,6 +1,6 @@
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import { defineConfig } from "astro/config";
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
