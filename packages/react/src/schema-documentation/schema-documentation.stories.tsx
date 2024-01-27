036362d0851c675f5b850bda27f753e724c3647e
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2024-01-27 16:14:31 -0600

    add SchemaDocumentation story

--- packages/react/src/schema-documentation/schema-documentation.stories.tsx
+++ packages/react/src/schema-documentation/schema-documentation.stories.tsx
@@ -4,6 +4,34 @@ import { SchemaView } from '../schema-view';
 import { SchemaDocumentation } from './components/schema-documentation';
 import { testSchema } from '@pathfinder-ide/stores/src/schema-store/test-schema';
 
+const overrides = {
+  dark: {
+    color: {
+      neutral: {
+        1: 'red',
+      },
+    },
+  },
+  light: {
+    color: {
+      neutral: {
+        1: 'blue',
+      },
+    },
+  },
+};
+
+export const WithSchemaAndThemeOptions = () => {
+  return (
+    <SchemaDocumentation
+      schema={testSchema}
+      themeOptions={{
+        overrides,
+      }}
+    />
+  );
+};
+
 export const WithSchema = () => {
   return <SchemaDocumentation schema={testSchema} />;
 };
