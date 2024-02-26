cedea615f06e2b08bf6449b70b6ace0d2ee58106
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2024-02-26 11:17:59 -0600

    add astro vercel package and adjust astro config

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,8 +1,12 @@
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  adapter: vercel({
+    imageService: true,
+  }),
 });
