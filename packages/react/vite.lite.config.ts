668d27912fc81a74570bb223997a0fcd5ef3bdf2
Mtg <mtg.software.dev@gmail.com>
2024-08-28 14:53:51 +0400

    use 'mergeConfig' instead of duplicating vite config files

--- packages/react/vite.lite.config.ts
+++ packages/react/vite.lite.config.ts
@@ -1,47 +1,17 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vitest/config';
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import dts from 'vite-plugin-dts';
-import pluginReact from '@vitejs/plugin-react';
+import { defineConfig, mergeConfig } from 'vitest/config';
+import defaultConfig from './vite.config';
 
-export default defineConfig({
-  // 👇 https://github.com/vitejs/vite/issues/11943#issuecomment-1419293906
-  base: './',
+const overrideConfig = defineConfig({
   define: {
     'import.meta.__IS_LITE_MODE_': 'true',
   },
   build: {
-    target: 'ESNext',
-    outDir: 'lite-dist',
-    lib: {
-      entry: 'src/index.ts',
-      name: 'pathfinder',
-      fileName: (format) => `pathfinder.${format}.js`,
-      formats: ['es'],
-    },
     rollupOptions: {
-      external: ['react', 'react-dom', 'graphql', /^monaco-editor(\/.*)?/],
-      output: {
-        format: 'esm',
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
+      external: [/^monaco-editor(\/.*)?/],
     },
-  },
-  plugins: [
-    dts({
-      rollupTypes: true,
-      exclude: ['./test'],
-    }),
-    pluginReact(),
-    vanillaExtractPlugin({}),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
   },
 });
+
+export default mergeConfig(defaultConfig, overrideConfig);
