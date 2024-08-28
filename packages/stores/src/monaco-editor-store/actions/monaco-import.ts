814a9562b77bbe95e30ae29fcf24d412410e1f70
Mtg <mtg.software.dev@gmail.com>
2024-08-28 13:07:21 +0400

    Add jsdoc for `setMonacoImporter`

--- packages/stores/src/monaco-editor-store/actions/monaco-import.ts
+++ packages/stores/src/monaco-editor-store/actions/monaco-import.ts
@@ -15,6 +15,21 @@ export function importMonaco() {
 
 let importFn: null | (() => Promise<typeof import('monaco-editor')>) = null;
 
+/**
+ * Sets the importer function for the 'monaco-editor' module.
+ *
+ * @param dynamicImport - a function that returns a promise that resolves to the 'monaco-editor' module.
+ *
+ * @remarks
+ * Only use this function if you are using `pathfinder-ide/react/lite`.
+ *
+ * @example
+ * ```tsx
+ * import { setMonacoImporter } from '@pathfinder-ide/stores';
+ *
+ * setMonacoImporter(() => import('monaco-graphql/esm/monaco-editor'));
+ * ```
+ */
 export function setMonacoImporter(
   dynamicImport: () => Promise<typeof import('monaco-editor')>,
 ) {
