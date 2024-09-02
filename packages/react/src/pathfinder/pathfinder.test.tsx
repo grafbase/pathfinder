7f5082bc69bebc259bec9eb896f179b2ea1a5fac
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2024-09-02 09:53:04 -0500

    cleanup test file

--- packages/react/src/pathfinder/pathfinder.test.tsx
+++ packages/react/src/pathfinder/pathfinder.test.tsx
@@ -1,4 +1,5 @@
-import { act, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { act } from 'react';
 import { Pathfinder } from './pathfinder';
 import { setTheme, themeStore } from '@pathfinder-ide/stores';
 import { testSchema } from '@pathfinder-ide/stores/src/schema-store/test-schema';
@@ -28,21 +29,22 @@ const overrides = {
 };
 
 describe('Pathfinder props', () => {
-  it('should correctly render Welcome when Pathfinder does not receive fetcherOptions', async () => {
+  it('should correctly render Welcome when Pathfinder does not receive fetcherOptions', () => {
     render(<Pathfinder />);
 
     const welcomeContainer = screen.getByTestId('welcome-container');
+
     expect(welcomeContainer).toBeInTheDocument();
   });
 
-  it('should correctly render Pathfinder without theme override props', async () => {
+  it('should correctly render Pathfinder without theme override props', () => {
     render(<Pathfinder fetcherOptions={{ endpoint: 'ENDPOINT' }} />);
 
     const themeOverrides = themeStore.getState().themeOverrides;
     expect(themeOverrides).toBe(null);
   });
 
-  it('should correctly render Pathfinder with theme override props', async () => {
+  it('should correctly render Pathfinder with theme override props', () => {
     render(
       <Pathfinder
         fetcherOptions={{ endpoint: 'ENDPOINT' }}
