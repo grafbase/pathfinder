afa7125ca918cbb6a0e7fa3ed8f1ce980864c0dc
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2024-11-04 12:41:58 -0600

    call lowercase() as we prep headers for sse client

--- packages/stores/src/session-store/slices/http-headers/actions/get-enabled-http-header-values.ts
+++ packages/stores/src/session-store/slices/http-headers/actions/get-enabled-http-header-values.ts
@@ -9,12 +9,14 @@ export const getEnabledHTTPHeaderValues: HTTPHeadersActions['getEnabledHTTPHeade
 
 export const getEnabledHTTPHeaderValueRecord: HTTPHeadersActions['getEnabledHTTPHeaderValueRecord'] =
   ({ headers }) => {
+    // we use this function to prepare headers for the graphql-sse client
+    // we're calling lowercase here to ensure that graphql-sse correctly merges headers ("Content-Type"): https://github.com/enisdenjo/graphql-sse/commit/0084de7c7f55c77c9b9156b98c264b90f49bf2b2
     return headers
       .filter((header) => header.enabled)
       .reduce(
         (accumulator, currentValue) => ({
           ...accumulator,
-          [currentValue.key]: currentValue.value,
+          [currentValue.key.toLowerCase()]: currentValue.value,
         }),
         {} as Record<string, string>,
       );
